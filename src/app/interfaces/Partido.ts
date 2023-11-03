73636418ee90fff92a2f1a234f36900e9032088f
CristianF1904 <cristian.ferreira1904@gmail.com>
2023-11-03 15:30:36 -0300

    Prueba_2

---
+++ src/app/interfaces/Partido.ts
@@ -0,0 +1,12 @@
+export interface Partido {
+  id: string;
+  nameGame: string;
+  nameCancha: string;
+  ubicacion: string;
+  p1: any;
+  p2: string;
+  p3: string;
+  p4: string;
+  date: string;
+  cantPlayers: string;
+}
---
+++ src/app/interfaces/user.ts
@@ -0,0 +1,8 @@
+export default interface User {
+    id?: string;
+    nombre: string;
+    apellido: string;
+    email: string;
+    password: string;
+    rol: any;
+}
